71ab16a48884c667efd07c5a6b3fbdd19e4a4a83
Abhishek12092002 <abhishekkumarshah12092001@gmail.com>
2024-05-28 23:14:29 +0530

    firebase uri updates

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrjHONqvT4iLAjkp0DhH-54a17vwq7uyU",
-  authDomain: "food-delivery-project-fea82.firebaseapp.com",
+  apiKey: "AIzaSyBpsL9HlgLfNEF0L_Y38P_VXi4q35l6Q2c",
+  authDomain: "gappebar-207a9.firebaseapp.com",
   databaseURL:
-    "https://food-delivery-project-fea82-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "food-delivery-project-fea82",
-  storageBucket: "food-delivery-project-fea82.appspot.com",
-  messagingSenderId: "487556154470",
-  appId: "1:487556154470:web:62183bc72def392aabd9a7",
+    "https://default.firebaseio.com",
+  projectId: "gappebar-207a9",
+  storageBucket: "gappebar-207a9.appspot.com",
+  messagingSenderId: "416532338157",
+  appId: "1:416532338157:web:015e6757aba156853043e4",
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
